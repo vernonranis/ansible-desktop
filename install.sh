65dd2a754dd77ff04181bd446deb4eb778d7b704
vernonranis <60091485+vernonranis@users.noreply.github.com>
2022-04-21 20:10:58 +0800

    fixed fedora docker install

--- install.sh
+++ install.sh
@@ -68,7 +68,8 @@ echo -e "${RED}Finished Installing NVM${NC}"
 
 echo -e "${RED}Installing Docker${NC}"
 sudo yum install -y yum-utils
-sudo dnf config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+sudo dnf -y install dnf-plugins-core
+sudo dnf config-manager --add-repo https://download.docker.com/linux/fedora/docker-ce.repo
 sudo dnf -y install docker-ce docker-ce-cli containerd.io
 sudo systemctl start docker
 sudo systemctl enable docker
