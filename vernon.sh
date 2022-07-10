6b9996f4109c992c6cca04dca0616a49b0a210c0
vernonranis <60091485+vernonranis@users.noreply.github.com>
2022-07-10 14:37:54 +0800

    Update vernon.sh

--- vernon.sh
+++ vernon.sh
@@ -1,3 +1,11 @@
+RED='\033[0;31m'
+NC='\033[0m' # No Color
+
+echo -e "${RED}whoami testing${NC}"
+whoami
+echo -e "${RED}finished whoami testing${NC}"
+
+
 echo -e "${RED}Installing PIP packages to user${NC}"
 pip install --user flake8
 pip install --user bandit
