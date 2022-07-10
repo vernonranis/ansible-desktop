cd6115cf5d4b9a98f689417a9243f12089302b37
vernonranis <60091485+vernonranis@users.noreply.github.com>
2022-07-10 17:00:56 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -92,11 +92,12 @@ sudo dnf install -y python3-psutil vlc vim-enhanced transmission-daemon transmis
 echo -e "${RED}Finished Installing Packages${NC}"
 
 echo -e "${RED}Installing vim plugins${NC}"
-sudo -u vernon mkdir -p ~/.vim/pack/flake8/start/
-sudo -u vernon cd ~/.vim/pack/flake8/start/
+sudo -u vernon mkdir -p /home/vernon/.vim/pack/flake8/start/
+sudo -u vernon cd /home/vernon/.vim/pack/flake8/start/
 sudo -u vernon git clone https://github.com/nvie/vim-flake8.git
 
 sudo -u vernon vim +PlugInstall +qall
+cd /home/vernon/
 echo -e "${RED}Finished Installing vim plugins${NC}"
 
 echo -e "${RED}Installing Brave Browser${NC}"
