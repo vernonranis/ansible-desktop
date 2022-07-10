3affab0c9738c8cec67b67f1d31a5f750f59fca4
vernonranis <60091485+vernonranis@users.noreply.github.com>
2022-07-10 14:53:02 +0800

    Update vernon.sh

--- vernon.sh
+++ vernon.sh
@@ -2,7 +2,9 @@ RED='\033[0;31m'
 NC='\033[0m' # No Color
 
 echo -e "${RED}whoami testing${NC}"
+cd ~
 whoami
+pwd
 echo -e "${RED}finished whoami testing${NC}"
 
 
@@ -10,6 +12,12 @@ echo -e "${RED}Installing PIP packages to user${NC}"
 pip install --user flake8
 pip install --user bandit
 echo -e "${RED}Finished Installing PIP packages to user${NC}"
+
+echo -e "${RED}whoami testing${NC}"
+cd ~
+whoami
+pwd
+echo -e "${RED}finished whoami testing${NC}"
 
 echo -e "${RED}Rebooting System${NC}"
 sudo reboot
